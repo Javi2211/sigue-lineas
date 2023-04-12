6cef439ec201ac71f1483cb7a5e47f9fce2928f5
Javi2211 <127950367+Javi2211@users.noreply.github.com>
2023-04-12 12:22:02 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -11,7 +11,7 @@ basic.forever(function () {
         maqueen.motorStop(maqueen.Motors.M2)
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, Velocidad)
     }
-    if (maqueen.Ultrasonic(PingUnit.Centimeters) > 7) {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 7) {
         if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
             maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, Velocidad)
         }
